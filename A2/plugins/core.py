ba8fc4a05f2442f4a6af3e80d26eb3e2ba8d5717
Ubuntu <ubuntu@ip-172-31-43-31.us-east-2.compute.internal>
2018-07-19 01:23:47 +0000

    Adding disco project files

---
+++ A2/plugins/core.py
@@ -0,0 +1,29 @@
+from disco.bot import Plugin
+from disco.bot.command import CommandLevels
+from disco.types.user import Game, GameType, Status
+
+
+class CorePlugin(Plugin):
+    @Plugin.listen('Ready')
+    def on_ready(self, ctx):
+        self.client.update_presence(Status.online, Game(type=GameType.listening, name='nothing because I don\'t work yet'))
+
+    @Plugin.command('reload')
+    def reload_command(self, event):
+        user_level = self.bot.get_level(event.author)
+        if user_level < CommandLevels.OWNER:
+            event.msg.reply('**Only admins can do that**')
+            return
+
+        self.log.info('Reloading Plugins')
+        reloaded_plugins = []
+        for name, value in self.bot.plugins.iteritems():
+            if 'Core' in name:
+                continue
+
+            self.log.info('reloading {}'.format(name))
+            self.bot.reload_plugin(value.__class__)
+            self.log.info('reloaded {}'.format(name))
+            reloaded_plugins.append(name)
+
+        event.msg.reply('Plugins reloaded:\n{}'.format('\n'.join(reloaded_plugins)))
