365550a745b8ca86962bae8daa3c908958592e62
MarkKoz <KozlovMark@gmail.com>
2018-07-29 00:21:35 -0700

    core: refactor
    * conform to an 80 column margin
    * add docstrings

--- A2/plugins/core.py
+++ A2/plugins/core.py
@@ -1,4 +1,7 @@
-#pylint:disable=E1101
+"""Core functionality of the bot.
+
+Handles bot initialisation, configuration, and 'lower-level' operations.
+"""
 from disco.bot import Plugin
 from disco.bot.command import CommandLevels
 from disco.types.user import Game, GameType, Status
@@ -7,25 +10,37 @@ from disco.types.user import Game, GameType, Status
 class CorePlugin(Plugin):
     @Plugin.listen('Ready')
     def on_ready(self, ctx):
-        self.client.update_presence(Status.online, Game(type=GameType.listening, name='nothing because I don\'t work yet'))
+        game = Game(
+            type=GameType.listening,
+            name='nothing because I don\'t work yet')
+        self.client.update_presence(Status.online, game)
 
     @Plugin.command('reload')
     def reload_command(self, event):
+        """= reload =
+        Reloads all plugins except Core.
+        usage    :: $reload
+        aliases  :: None
+        category :: Core
+        """
         user_level = self.bot.get_level(event.author)
+
         if user_level < CommandLevels.OWNER:
-            event.msg.reply('**Only admins can do that**')
+            event.msg.reply('**Only admins can do that!**')
             return
 
         self.log.info('Reloading Plugins')
         reloaded_plugins = []
-        for name, value in self.bot.plugins.copy().items():
+
+        for name, instance in self.bot.plugins.copy().items():
             # Don't reload Core.
-            if value == self:
+            if instance == self:
                 continue
 
-            self.log.info('reloading {}'.format(name))
-            self.bot.reload_plugin(value.__class__)
-            self.log.info('reloaded {}'.format(name))
+            self.log.info('Reloading {}.'.format(name))
+            self.bot.reload_plugin(instance.__class__)
+            self.log.info('Reloaded {}.'.format(name))
             reloaded_plugins.append(name)
 
-        event.msg.reply('Plugins reloaded:\n{}'.format('\n'.join(reloaded_plugins)))
+        reloaded_plugins = '\n'.join(reloaded_plugins)
+        event.msg.reply('Plugins reloaded:\n```{}```'.format(reloaded_plugins))
